05e14ae393291a84f8de105fefd9d6460a0f06d5
adarshraj007 <adarshraj007@gmail.com>
2020-11-26 15:10:00 +0530

    schema updated.

--- app.py
+++ app.py
@@ -38,56 +38,12 @@ def token_required(f):
     return decorated
 
 
-@app.route('/customer/<int:customer_id>', methods=['GET'])
-@token_required
-def get_customer_detail(customer_id):
-    customer = Customer(customer_id)
-    return customer.get_customer_details('accountNumber', customer_id)
-
-
 @app.route('/register', methods=["POST"])
 @app.validate('customer', 'add')  # file name, schema name
 def register_customer():
     data = request.json
     customer = Customer(data)
     return customer.add_customer()
-
-
-@app.route('/loan/<loan_id>', methods=["GET"])
-@token_required
-def loan_detail(loan_id):
-    customer = Customer(loan_id)
-    return customer.get_loan_details()
-
-
-@app.route('/customer', methods=['POST'])
-@token_required
-def update_account_detail():
-    data = request.json
-    customer = Customer(data)
-    return customer.update_account_detail()
-
-
-@app.route('/customer/<int:customer_id>', methods=['DELETE'])
-@token_required
-def delete_customer(customer_id):
-    customer = Customer(customer_id)
-    delete_response = customer.delete_customer(customer_id)
-    if int(delete_response) == 0:
-        return make_response('Could not delete: User not found', 404)
-    elif int(delete_response):
-        return make_response('Deleted', 200)
-
-
-@app.route('/loan/<loan_id>', methods=['DELETE'])
-@token_required
-def delete_loan(loan_id):
-    customer = Customer(loan_id)
-    delete_response = customer.delete_customer(loan_id)
-    if int(delete_response) == 0:
-        return make_response('Could not delete: User not found', 404)
-    elif int(delete_response):
-        return make_response('Deleted', 200)
 
 
 @app.route('/login', methods=['POST'])
@@ -129,6 +85,32 @@ def login():
         )
 
 
+@app.route('/customer', methods=['POST'])
+@token_required
+def update_account_detail():
+    data = request.json
+    customer = Customer(data)
+    return customer.update_account_detail()
+
+
+@app.route('/customer/<int:customer_id>', methods=['GET'])
+@token_required
+def get_customer_detail(customer_id):
+    customer = Customer(customer_id)
+    return customer.get_customer_details('accountNumber', customer_id)
+
+
+@app.route('/customer/<int:customer_id>', methods=['DELETE'])
+@token_required
+def delete_customer(customer_id):
+    customer = Customer(customer_id)
+    delete_response = customer.delete_customer(customer_id)
+    if int(delete_response) == 0:
+        return make_response('Could not delete: User not found', 404)
+    elif int(delete_response):
+        return make_response('Deleted', 200)
+
+
 @app.route('/loan', methods=['POST'])
 @app.validate('loan', 'add')  # file name, schema name
 @token_required
@@ -136,6 +118,24 @@ def add_loan_details():
     data = request.json
     customer = Customer(data)
     return customer.add_loan_details()
+
+
+@app.route('/loan/<loan_id>', methods=["GET"])
+@token_required
+def loan_detail(loan_id):
+    customer = Customer(loan_id)
+    return customer.get_loan_details()
+
+
+@app.route('/loan/<loan_id>', methods=['DELETE'])
+@token_required
+def delete_loan(loan_id):
+    customer = Customer(loan_id)
+    delete_response = customer.delete_customer(loan_id)
+    if int(delete_response) == 0:
+        return make_response('Could not delete: User not found', 404)
+    elif int(delete_response):
+        return make_response('Deleted', 200)
 
 
 if __name__ == '__main__':
