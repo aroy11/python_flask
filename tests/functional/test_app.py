7964d43bf31b9332c7775da95f70e73573692bf5
Sanooj <sanooj@outlook.com>
2020-12-21 10:33:07 +0530

    pyTest for add customer and get loan details

--- tests/functional/test_app.py
+++ tests/functional/test_app.py
@@ -1,11 +1,13 @@
 import json
 
+
 def test_customer_login_success(test_client):
-    response = test_client.post('/login', json={ 'username' :'johndoe','password': 'Welcome'})
+    response = test_client.post('/login', json={'username': 'johndoe', 'password': 'Welcome'})
     assert response.status_code == 201
 
+
 def test_customer_login_invalid_user_detail(test_client):
-    response = test_client.post('/login', json={ 'username1' :'test01','password': 'Test0101'})
+    response = test_client.post('/login', json={'username1': 'test01', 'password': 'Test0101'})
     assert response.status_code == 401
 
 
@@ -23,6 +25,25 @@ def test_customer_get_details_missing_identifier(test_client, test_token):
 
 def test_customer_get_details_no_record_in_db(test_client, test_token):
     url_path = '/customer?accountNumber=800'
+    get_response = test_client.get(url_path, headers={'Authorization': test_token})
+    assert get_response.status_code == 200
+    assert json.loads(get_response.data)['message'] == 'No records found'
+
+
+def test_get_loan_details_success(test_client, test_token):
+    url_path = '/loan?loanID=1001'
+    get_response = test_client.get(url_path, headers={'Authorization': test_token})
+    assert get_response.status_code == 200
+
+
+def test_get_loan_details_missing_identifier(test_client, test_token):
+    url_path = '/loan'
+    get_response = test_client.get(url_path, headers={'Authorization': test_token})
+    assert get_response.status_code == 404
+
+
+def test_get_loan_details_no_record_found(test_client, test_token):
+    url_path = '/loan?loanID=2020'
     get_response = test_client.get(url_path, headers={'Authorization': test_token})
     assert get_response.status_code == 200
     assert json.loads(get_response.data)['message'] == 'No records found'
--- tests/unit/test_customer_service.py
+++ tests/unit/test_customer_service.py
@@ -10,15 +10,21 @@ class MockGetResponse:
                  'password': 'pbkdf2:sha256:150000$DBlDG8RX$b54055d6fefb40a2272c73ff7add04af9dcd332e880e3d4519848c37e754d0de',
                  'FirstName': 'John', 'accountNumber': 1001}]
 
+
 class MockGetCustomerResponse:
     @staticmethod
     def get(data):
-        return [{'UserName': 'jhonnytwo', 'password': 'pbkdf2:sha256:150000$aVIJU0sw$edd225a7253f4e98f29a635cb0fff37aa23379a4a570471588c3cbedc0cd5ecf', 'FirstName': 'Ben', 'accountNumber': 1002}]
+        return [{'UserName': 'jhonnytwo',
+                 'password': 'pbkdf2:sha256:150000$aVIJU0sw$edd225a7253f4e98f29a635cb0fff37aa23379a4a570471588c3cbedc0cd5ecf',
+                 'FirstName': 'Ben', 'accountNumber': 1002}]
+
 
 class MockGetEmptyCustomerResponse:
     @staticmethod
     def get(data):
         return []
+
+
 class MockGetEmptyResponse:
     @staticmethod
     def values():
@@ -95,7 +101,6 @@ class TestCustomerService:
         monkeypatch.setattr(customer.mongo_repository, "get_record", mock_get)
         return customer
 
-
     @pytest.fixture
     def return_valid_accountnumber_response(self, monkeypatch):
         customer = customer_service.Customer()
@@ -140,6 +145,20 @@ class TestCustomerService:
             response = customer.get_customer_details('accountNumber', 1002, True)
             assert response.json['message'] == 'Invalid search condition'
 
+    def test_get_loan_details_success_account_number(self, return_valid_loan_response):
+        response = return_valid_loan_response.get_loan_details('loanID', 1001)
+        assert response is not None
+
+    def test_get_loan_details_without_results(self, return_empty_response):
+        app = Flask(__name__)
+        with app.app_context():
+            response = return_empty_response.get_loan_details('loanID', 2020, True)
+            assert response.json['message'] == 'No records found'
+
+    def test_get_loan_details_without_results_without_message(self, return_empty_response):
+        response = return_empty_response.get_loan_details('loanID', 1001)
+        assert response is not None
+
     def test_login_valid_credentials_should_return_valid_jwt_token(self, return_valid_response_login):
         app = Flask(__name__)
         app.config['SECRET_KEY'] = 'test'
@@ -161,16 +180,3 @@ class TestCustomerService:
             response = invalid_password_login.login()
             assert response.headers['WWW-Authenticate'] == 'Invalid credentials'
             assert response.status_code == 403
-    def test_get_loan_details_success_account_number(self, return_valid_loan_response):
-        response = return_valid_loan_response.get_loan_details('loanID', 1001)
-        assert response is not None
-
-    def test_get_loan_details_without_results(self, return_empty_response):
-        app = Flask(__name__)
-        with app.app_context():
-            response = return_empty_response.get_loan_details('loanID', 1005, True)
-            assert response.json['message'] == 'No records found'
-
-    def test_get_loan_details_without_results_without_message(self, return_empty_response):
-        response = return_empty_response.get_loan_details('loanID', 1001)
-        assert response is not None
